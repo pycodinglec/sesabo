435ccd1f3a948ea80bb3ef269a3129a81ccbbfae
KYUHO LEE <pycodinglec@naver.com>
2023-10-16 01:37:27 +0900

    initial upload(2)

---
+++ documents/legacy/Academic_Grade_Management_Regulations/Academic_Grade_Management_Regulations.py
@@ -0,0 +1,30 @@
+"""
+세화고등학교 학업성적관리규정
+"""
+
+
+
+class 호:
+    def __init__(self):
+        pass
+
+class 항:
+    def __init__(self):
+        pass
+
+class 조:
+    def __init__(self):
+        pass
+
+class 장:
+    def __init__(self):
+        pass
+
+class 규정:
+    def __init__(self):
+        pass
+
+
+
+
+
